204b7d823f87a1c8b394ce6aea49aa837c10fde2
Sami Munir <samidmunir@outlook.com>
2026-03-03 21:21:28 -0500

    creating first selenium java maven project for UI testing orangehrm

---
+++ orangehrm/src/main/java/com/example/Main.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\nOrangeHRM Selenium Test\n-------------------------\n");
+    }
+}
