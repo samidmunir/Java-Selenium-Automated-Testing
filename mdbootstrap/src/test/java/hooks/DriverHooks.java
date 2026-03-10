edf34a066081b171c45b911f77cfa12564c1fd92
Sami Munir <samidmunir@outlook.com>
2026-03-10 15:49:42 -0400

    implementing DriverHooks for DriverFactory design

--- mdbootstrap/src/test/java/hooks/DriverHooks.java
+++ mdbootstrap/src/test/java/hooks/DriverHooks.java
@@ -1,16 +1,25 @@
 package hooks;
 
+import org.openqa.selenium.WebDriver;
+
+import factory.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import utils.DriverManager;
+import utils.ProcessConfig;
 
 public class DriverHooks {
     @Before
     public void init() {
-        //
+        String browser = ProcessConfig.getProperty("browser");
+        WebDriver driver = DriverFactory.initializeDriver(browser);
+        DriverManager.setDriver(driver);
+
+        driver.manage().window().maximize();
     }
 
     @After
     public void tearDown() {
-        //
+        DriverManager.quitDriver();
     }
 }
