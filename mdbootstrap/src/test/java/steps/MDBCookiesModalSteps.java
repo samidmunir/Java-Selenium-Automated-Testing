e2f59a463a612cab5274f9a41cdcbad8a4f2e28d
Sami Munir <samidmunir@outlook.com>
2026-03-10 21:24:01 -0400

    implementing BDD using Cucumber & Gherkins, creating step definitions & test runner for MDB accept cookies alert test

---
+++ mdbootstrap/src/test/java/steps/MDBCookiesModalSteps.java
@@ -0,0 +1,32 @@
+package steps;
+
+import org.testng.Assert;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.MDBDropdownPage;
+
+public class MDBCookiesModalSteps {
+    private MDBDropdownPage page = new MDBDropdownPage();
+
+    @Given("the user is on the MDB multi-level dropdown page")
+    public void theUserIsOnTheMDBMultiLevelDropdownPage() {
+        page.openPage();
+    }
+
+    @Then("the cookie modal should be visible")
+    public void theAcceptCookiesModalShouldBeVisible() {
+        Assert.assertTrue(page.isAcceptCookiesModalVisible(), "Expected the accept cookies modal to be visible.");
+    }
+
+    @When("the user clicks the Accept Cookies button")
+    public void theUserClicksTheAcceptCookiesButton() {
+        page.clickAcceptCookiesButton();
+    }
+
+    @Then("the cookie modal should no longer be visible")
+    public void theAcceptCookiesModalShouldNoLongerBeVisible() {
+        Assert.assertTrue(page.isAcceptCookiesModalGone(), "Expected the accept cookies modal to disappear after clicking Accept Cookies.");
+    }
+}
